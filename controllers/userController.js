d9edf2e332a36136945f17d780fd07e0c6655a29
hourglass2020 <pouria.fm15@gmail.com>
2023-04-04 19:46:42 +0430

    compelete user login controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,8 @@
+const jwt = require("jsonwebtoken");
+
 const {createError} = require("../utils/errorParser");
 const User = require("../models/user");
+const bcrypt = require("bcryptjs");
 
 exports.createUser = async (req, res, next) => {
     try {
@@ -25,6 +28,37 @@ exports.createUser = async (req, res, next) => {
 
         // return res.status(422).json({message: "This email is already used."});
         throw createError("This email is already used.", 422);
+    } catch (err) {
+        next(err);
+    }
+}
+
+exports.handleLogin = async (req, res, next) => {
+    const {password, email} = req.body;
+
+    try {
+        const user = await User.findOne({where: {email}});
+
+        if (user === null) {
+            throw createError("there is not such a user.", 404);
+        }
+
+        let isEqual = await bcrypt.compare(password, user.password);
+
+        if (isEqual) {
+            const token = jwt.sign({
+                user: {
+                    userId: user.id,
+                    fullName: user.fullName,
+                    email: user.email
+                }
+            }, process.env.JWT_SECRET);
+
+            return res.status(200).json({token, userId: user.id});
+        }
+
+        throw createError("Password is not correct!", 422);
+
     } catch (err) {
         next(err);
     }
--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,8 @@
 const express = require("express");
-const bcrypt = require("bcryptjs");
 
 const userController = require("../controllers/userController");
-const User = require("../models/user");
-
-const {createError} = require("../utils/errorParser");
 
 const router = express.Router();
-
 
 // @desc    get all users
 // @route   GET /user/all
@@ -19,27 +14,6 @@ router.post("/register", userController.createUser);
 
 // @desc    login handle
 // @route   POST /user/login
-router.post("/login", async (req, res, next) => {
-    const {password, email} = req.body;
-
-    try {
-        const user = await User.findOne({where: {email}});
-
-        if (user === null) {
-            throw createError("there is not such a user.", 404);
-        }
-
-        let isEqual = await bcrypt.compare(password, user.password);
-
-        if (isEqual) {
-            return res.status(200).json({message: "Successfully logged in."});
-        }
-
-        throw createError("Password is not correct!", 422);
-
-    } catch (err) {
-        next(err);
-    }
-})
+router.post("/login", userController.handleLogin);
 
 module.exports = router;
