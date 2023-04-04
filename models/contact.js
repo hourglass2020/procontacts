5390b3cb8583981213e3167d7e3ca29069a59642
hourglass2020 <pouria.fm15@gmail.com>
2023-04-04 20:22:17 +0430

    define contact and label models

---
+++ models/contact.js
@@ -0,0 +1,52 @@
+const sequelize = require("../config/databse");
+const {DataTypes} = require("sequelize");
+
+const User = require("./user");
+
+const Contact = sequelize.define("Contact", {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    firstName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    lastName: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    company: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    phone: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    phoneLabel: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    emailLabel: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    website: {
+        type: DataTypes.STRING,
+        allowNull: true
+    }
+})
+
+Contact.belongsTo(User);
+User.hasMany(Contact, {
+    foreignKey: "phonebook"
+});
+
+module.exports = Contact;
---
+++ models/label.js
@@ -0,0 +1,23 @@
+const {DataTypes} = require("sequelize");
+
+const sequelize = require("../config/databse");
+
+const Contact = require("./contact");
+
+const Label = sequelize.define("Lable", {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+})
+
+Label.belongsToMany(Contact);
+Contact.hasMany(Label);
+
+module.exports = Label;
